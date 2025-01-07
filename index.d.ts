3994642cde1db37e4811cffeb1b97c06e5dd5dc8
Gleb Fomin <gleb.fom28@gmail.com>
2025-01-07 15:43:14 +0500

    fix imports

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,8 @@
-type SpecialCases = Record<number, string>;
+declare module 'pluralize-tool' {
+  type SpecialCases = Record<number, string>;
+  type Titles = string[];
 
-type Titles = string[];
+  function plural(number: number, titles: Titles, specialCases?: SpecialCases): string;
 
-export function plural(number: number, titles: Titles, specialCases?: SpecialCases): string;
+  export default plural;
+}
